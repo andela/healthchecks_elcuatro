c6fb597d3212c251cf9b5acb6e412a74a43a0dd9
Luke Olali <luke.olali@andela.com>
2016-11-28 12:58:09 +0300

    [Chore #134841633] - Adds database configuration settings for production environment

--- hc/settings/production.py
+++ hc/settings/production.py
@@ -6,8 +6,11 @@ DEBUG = False
 
 # Update database configuration with $DATABASE_URL
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES = {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+}
 
+ALLOWED_HOSTS = ['https://hc-elcuatro-dev.herokuapp.com/']
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
