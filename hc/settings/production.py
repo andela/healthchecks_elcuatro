911ea6c95eb619e0a873a157901aa041af7a0688
Luke Olali <luke.olali@andela.com>
2016-11-28 13:48:40 +0300

    [Chore #134841633] - Adds secure proxy SSL header

--- hc/settings/production.py
+++ hc/settings/production.py
@@ -10,6 +10,7 @@ DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['https://hc-elcuatro-dev.herokuapp.com/']
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
