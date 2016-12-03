10e6a54880d428536db9fd8f43013076ba29870d
Luke Olali <luke.olali@andela.com>
2016-12-03 19:43:26 +0300

    [Chore #134841633] - Changes base directory path

--- hc/settings.py
+++ hc/settings.py
@@ -15,7 +15,7 @@ import warnings
 import dj_database_url
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 HOST = "localhost"
 SECRET_KEY = "---"
