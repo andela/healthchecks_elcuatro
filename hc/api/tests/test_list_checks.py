f8989f28f6abe5a8865fdb54c0d0844412466a44
andela-andrewmakenzi <andrew.makenzi@andela.com>
2016-12-14 17:50:28 +0300

    [ft#1338348327] checks Alice 1 and Alice 2's timeout, grace, ping_url, status

--- hc/api/tests/test_list_checks.py
+++ hc/api/tests/test_list_checks.py
@@ -34,13 +34,23 @@ class ListChecksTestCase(BaseTestCase):
     def test_it_works(self):
         r = self.get()
         ### Assert the response status code
+        self.assertEqual(r.status_code,200)
 
         doc = r.json()
         self.assertTrue("checks" in doc)
 
         checks = {check["name"]: check for check in doc["checks"]}
         ### Assert the expected length of checks
+        self.assertEqual(len(checks), 2)
         ### Assert the checks Alice 1 and Alice 2's timeout, grace, ping_url, status,
+        self.assertEqual(checks['Alice 1']['timeout'], 3600)
+        self.assertEqual(checks['Alice 1']['grace'], 900)
+        self.assertEqual(checks['Alice 1']['ping_url'], self.a1.url())
+        self.assertEqual(checks['Alice 1']['status'], 'new')
+        self.assertEqual(checks['Alice 2']['timeout'], 86400)
+        self.assertEqual(checks['Alice 2']['grace'], 3600)
+        self.assertEqual(checks['Alice 2']['ping_url'], self.a2.url())
+        self.assertEqual(checks['Alice 2']['status'], 'up')
         ### last_ping, n_pings and pause_url
 
     def test_it_shows_only_users_checks(self):
